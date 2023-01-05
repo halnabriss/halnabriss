70fc3f28dd9d7404e48d94202fe04130ef487f40
hadi alnabriss <alnabris@gmail.com>
2023-01-05 09:56:50 +0200

    Built Mobile class

---
+++ main.py
@@ -0,0 +1,5 @@
+from mobile import *
+
+my_mobile =  Mobile("Samsung", "white")
+
+print(my_mobile.color)
---
+++ mobile.py
@@ -0,0 +1,6 @@
+
+
+class Mobile:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
