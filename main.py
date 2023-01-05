dd52c425c579ba33921e81997244de8171772ce7
Hadi Alnabriss <54836977+halnabriss@users.noreply.github.com>
2023-01-05 10:56:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ my_car = Car("Mazda", "White", 2 , 1992)
 
 print(my_mobile.color)
 print(my_mobile.cpu_cores)
-print("My car age is: " + str(my_car.findAge()))
+print("My car age is : " + str(my_car.findAge()))
